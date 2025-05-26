2ba95dd22ce90462a44c4b7138426cbd5f60c7ee
marcoperuzzzocmd <marcoantperuzzo@gmail.com>
2025-05-26 16:44:29 -0300

    feat: alterando a api

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .categorias import Categorias
 from .editora import Editora
+from .autor import Autor
 from uploader.models import Image
 
 class Livro(models.Model):
@@ -10,6 +11,8 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     categoria = models.ForeignKey(Categorias,on_delete=models.PROTECT,related_name="livros", null=True, blank=True,)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
+    autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
+
     capa = models.ForeignKey(
         Image,
         related_name="+",
@@ -20,4 +23,4 @@ class Livro(models.Model):
     )
 
     def __str__(self):
-        return f"({self.id}) {self.titulo} {self.quantidade}"
+        return f"({self.id}) {self.titulo} {self.quantidade} - {self.autor}"
